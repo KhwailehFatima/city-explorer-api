39ab05f8939565303cc21833f4b5cf40ac9241c4
user name <user emaile>
2022-08-08 00:10:33 +0300

    adding a cache memory to the server

--- module/weather.js
+++ module/weather.js
@@ -1,21 +1,30 @@
 const axios = require('axios');
 
+const weatherCach={};
+
 async function handleWeather(req,res){
   // let searchQuery = req.query.searchQuery; //let {searchQuery} = req.query
   const {searchQuery,lat,lon}=req.query;
-  const weatherURL=`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&query=${searchQuery}`
-  const cityWeather=await axios.get(weatherURL);
+  
+  if (weatherCach[searchQuery]!==undefined){
+    res.status(200).send(weatherCach[searchQuery]);
+  } else{
 
-  try {
-    const weatherArr = cityWeather.data.data.map(item => new Forecast(item));
-    console.log(cityWeather)
-    res.status(200).send(weatherArr);
-
-  } catch (err) {
-    res.status(400).send(error)
+    const weatherURL=`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&query=${searchQuery}&lat=${lat}&lon=${lon}`
+    const cityWeather=await axios.get(weatherURL);
+  
+    try {
+      const weatherArr = cityWeather.data.data.map(item => new Forecast(item));
+      console.log(cityWeather)
+      res.status(200).send(weatherArr);
+  
+    } catch (err) {
+      res.status(400).send(error)
+    }
   }
-}
+  }
 
+  
 // class to take the data from weather api
 class Forecast {
   constructor(day) {
